3b5a4e9bec3efe173f590729f734173057b30581
오지훈 <162779852+appleitsgood@users.noreply.github.com>
2025-10-10 22:49:51 +0900

    Feat: implement from(), pathsFromStart, pathsToGoal, solution

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -40,8 +40,9 @@ trait Solver extends GameDef {
    */
   def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
                        explored: Set[Block]): Stream[(Block, List[Move])] = {
-    neighbors.filter { case (block, moves) => !explored.contains(block)
+    neighbors.filter { case (block, _) => !explored.contains(block)
     }
+  }
 
   /**
    * The function `from` returns the stream of all possible paths
@@ -67,18 +68,24 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] = initial match {
+    case Stream.Empty => Stream.Empty
+    case (block, moves) #:: rest =>
+      val next = newNeighborsOnly(neighborsWithHistory(block, moves), explored)
+
+      (block, moves) #:: from(rest #::: next, explored + block)
+  }
 
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = from(Stream((startBlock, Nil)), Set(startBlock))
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] = pathsFromStart.filter {case (block, _) => done(block) }
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -88,5 +95,5 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = pathsToGoal.head._2.reverse
 }
