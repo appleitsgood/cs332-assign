6a38adb694ce528c1e747d592afe96fe6edfdced
오지훈 <162779852+appleitsgood@users.noreply.github.com>
2025-11-08 18:37:10 +0900

    Feat: Implement combinators

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -101,7 +101,10 @@ package object nodescala {
      *  However, it is also non-deterministic -- it may throw or return a value
      *  depending on the current state of the `Future`.
      */
-    def now: T = ???
+    def now: T = {
+      if(!f.isCompleted) throw new NoSuchElementException("Future not completed")
+      Await.result(f, Duration.Zero)
+    }
 
     /** Continues the computation of this future by taking the current future
      *  and mapping it into another future.
@@ -109,7 +112,16 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continueWith[S](cont: Future[T] => S): Future[S] = ???
+    def continueWith[S](cont: Future[T] => S): Future[S] = {
+      val p = Promise[S]()
+
+      f.onComplete { _ =>
+        p.success(cont(f))
+      }
+
+      p.future
+    }
+
 
     /** Continues the computation of this future by taking the result
      *  of the current future and mapping it into another future.
@@ -117,7 +129,14 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continue[S](cont: Try[T] => S): Future[S] = ???
+    def continue[S](cont: Try[T] => S): Future[S] = {
+      val p = Promise[S]()
+
+      f.onComplete { t =>
+        p.success(cont(t))
+      }
+      p.future
+    }
 
   }
 
